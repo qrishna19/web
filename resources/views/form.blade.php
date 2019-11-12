87ac38abd00f3030449543576fc2e9e0a02ee9de
qrishna19 <qrishnaakbar@mail.ugm.ac.id>
2019-11-12 08:32:55 +0700

    web

---
+++ resources/views/form.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Produk</title>
+</head>
+<body>
+ 
+	<h2>PRODUK</h2>
+	<br/>
+	<a href="index.php">KEMBALI</a>
+	<br/>
+	<br/>
+	<h3>TAMBAH PRODUK</h3>
+	<form method="post" action="tambah_aksi.php">
+		<table>
+			<tr>			
+				<td>Nama</td>
+				<td><input type="text" name="nama"></td>
+			</tr>
+			<tr>
+				<td>Merk</td>
+				<td><input type="text" name="merk"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" value="SIMPAN"></td>
+			</tr>		
+		</table>
+	</form>
+</body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>tes</title>
+</head>
+<body>
+<h1>TES PAGE</h1>
+</body>
+</html>
